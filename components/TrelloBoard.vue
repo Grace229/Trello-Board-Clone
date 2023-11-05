b5568bab9da76901559b199de999cb8c1c3d915e
Grace Itiung <Grace.Itiung@Sterling.ng>
2023-11-05 22:41:49 +0100

    sort the columns with vue draggable

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -2,6 +2,7 @@
 import type { Column } from '../types/index.ts';
 import { nanoid } from "nanoid";
 import TrelloBoardTask from './TrelloBoardTask.vue';
+import draggable from 'vuedraggable';
 const columns = ref<Column[]>([
     {
         id: nanoid(),
@@ -24,23 +25,34 @@ const columns = ref<Column[]>([
             },
         ]
     },
-    {id: nanoid(), title: "Selected for Dev", tasks: []},
-    {id: nanoid(), title: "In Progress", tasks: []},
-    {id: nanoid(), title: "QA", tasks: []},
-    {id: nanoid(), title: "Complete", tasks: []},
+    { id: nanoid(), title: "Selected for Dev", tasks: [] },
+    { id: nanoid(), title: "In Progress", tasks: [] },
+    { id: nanoid(), title: "QA", tasks: [] },
+    { id: nanoid(), title: "Complete", tasks: [] },
 ])
 
 </script>
 <template>
     <div class="flex gap-4 overflow-x-auto items-start">
-        <div v-for="column in columns" :key="column.id" class="column bg-gray-200 p-5 rounded min-w-[250px]">
-<header class="font-bold">
-    {{ column.title }}
-</header>
-<TrelloBoardTask v-for="task in column.tasks" :task="task"/>
-<footer>
-    <button class="text-gray-500">Add a Card</button>
-</footer>
+        <draggable 
+        v-model="columns" 
+        group="columns" 
+        item-key="id"
+        class="flex gap-4 overflow-x-auto items-start"
+        >
+        <template #item="{element: column} : {element: Column}">
+            <div  class="column bg-gray-200 p-5 rounded min-w-[250px]">
+            <header class="font-bold mb-2">
+                {{ column.title }}
+            </header>
+            <TrelloBoardTask
+            v-for="task in column.tasks" 
+            :task="task" />
+            <footer>
+                <button class="text-gray-500">Add a Card</button>
+            </footer>
         </div>
+        </template>
+        </draggable>
     </div>
 </template>
